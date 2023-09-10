0abbe530c50a163ebc1ad8bead5bdf8902c692f3
nzlongboi <116671677+nzlongboi@users.noreply.github.com>
2023-09-10 22:11:47 +1200

    Add for loop to generate 16x16 grid. Work to turn tiles black on click

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-//js
+
+
+// a loop to create my grid
+// I guess having two variables to get a grid size is unneccesary 
+// but right now it gets the job done
+// I may have to fiddle with them a bit when I get around to 
+// changing the grid size
+
+var gridContainer = document.getElementById('gridContainer'); // getting our gridContainer element from our html
+var gridColumns = 16;
+var gridRows = 16;
+var gridSize = gridRows * gridColumns; // our number to intitialise the grid. 
+
+for(var i = 0; i < gridSize; i++) {
+    var gridTiles = document.createElement('div');
+    gridTiles.className = 'gridTiles';
+
+      
+    gridContainer.appendChild(gridTiles);
+}
+
+ 
+// This configuration of the loop generates 256 gridTiles which
+// is exactly what I want. I just have to figure out how to make them fit
+// I am trying to set their size to 100% but they overflow from their container
+// I can set them to 35px by 35px and they all fit inside the 
+// container, but how will I make their size variable for the 
+// grid size selector?
+
+// Do I need a loop that can change? A variable loop?
+// Im using a for loop here, do I need a while loop?
+// while(... i dont remember the syntax right now and I dont have internet)
+// Well at least I have a section of code that works how I need it to
+
+
+
+// For the next part of the project, I need to research css animations
+// I need to add event listeners to the tiles and make them turn black 
+// and stay black once clicked. I did this with DOM methods last time
+// But maybe I will just use css methods to make things simpler
+
+
+
+
+// I need to find a way to add event listeners to every
+// iteration of gridTiles
+// So, having asked ChatGPT some questions, I see that the forEach()
+// method can be used here. It looks like I can essentially create and
+// initialise an array in order to use the forEach method
+
+var tileArray = document.querySelectorAll('gridTiles');
+
+tileArray.forEach
+
